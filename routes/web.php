1fedf32393869c8185ab549b1d56d4cccc008e1a
Agustin Bettig <agustinbettigyt@gmail.com>
2023-03-27 18:49:50 -0300

    product crud

--- routes/web.php
+++ routes/web.php
@@ -21,16 +21,21 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::view('/products/index', 'products.index');
-Route::view('/products/create', 'products.create');
-Route::view('/products/edit', 'products.edit');
+
 Route::view('/products/stats', 'products.stats');
 Route::view('/dealers/details', 'dealers.details');
 Route::view('/dealers/edit', 'dealers.edit');
 Route::view('/routes/details', 'routes.details');
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+    Route::view('/products/create', 'products.create');
+    Route::view('/products/edit', 'products.edit');
+    Route::get('/product/index', [App\Http\Controllers\ProductController::class, 'index']);
     Route::post('/product/create', [App\Http\Controllers\ProductController::class, 'store']);
+    Route::post('/product/edit', [App\Http\Controllers\ProductController::class, 'store']);
+
 });
 
 
