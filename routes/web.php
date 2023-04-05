bbaf8026d9b365bf09bdaf18d2da1458b85beb8a
MartinSola55 <martinrsola55@gmail.com>
2023-04-05 19:57:22 -0300

    web.php changed

--- routes/web.php
+++ routes/web.php
@@ -20,24 +20,28 @@ Auth::routes();
 
 Route::middleware(['auth', 'admin'])->group(function () {
 
-    Route::view('/product/stats', 'products.stats');
+    Route::view('/dealers/index', 'dealers.index');
     Route::view('/dealer/details', 'dealers.details');
     Route::view('/dealer/edit', 'dealers.edit');
-    Route::view('/route/details', 'routes.details');
-
-    Route::view('/product/create', 'products.create');
-    Route::view('/product/edit', 'products.edit');
+    
+    
     Route::get('/product/index', [App\Http\Controllers\ProductController::class, 'index']);
     Route::post('/product/create', [App\Http\Controllers\ProductController::class, 'store']);
     Route::post('/product/edit', [App\Http\Controllers\ProductController::class, 'update']);
-
+    Route::view('/product/stats', 'products.stats');
+    
     Route::get('/client/index', [App\Http\Controllers\ClientController::class, 'index']);
     Route::post('/client/create', [App\Http\Controllers\ClientController::class, 'store']);
     Route::post('/client/edit', [App\Http\Controllers\ClientController::class, 'update']);
-
+    Route::view('/clients/details', 'clients.details');
+    Route::view('/clients/invoice', 'clients.invoice');
+    
     Route::get('/route/index', [App\Http\Controllers\RouteController::class, 'index']);
     Route::post('/route/create', [App\Http\Controllers\RouteController::class, 'store']);
     Route::post('/route/edit', [App\Http\Controllers\RouteController::class, 'update']);
+    Route::view('/route/details', 'routes.details');
+    Route::view('/routes/cart', 'routes.cart');
+    Route::view('/routes/details', 'routes.details');
 
     Route::get('/cart/index', [App\Http\Controllers\CartController::class, 'index']);
     Route::post('/cart/create', [App\Http\Controllers\CartController::class, 'store']);
