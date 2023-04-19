32f6ca57f3f97303c86adf881b427206e2736f1e
Martín Sola <martinrsola55@gmail.com>
2023-04-19 11:30:36 -0300

    Deleted comments

--- app/Models/Route.php
+++ app/Models/Route.php
@@ -27,31 +27,6 @@ class Route extends Model
 
     public function Info()
     {
-        /*$index = 0;
-        $info = [];
-        $info['state'] = "En depósito";
-        $info['total_carts'] = $this->Carts()->count();
-        $info['completed_carts'] = 0;
-        $info['total_collected'] = 0;
-        $countCarts = 0;
-        $carts = $this->with(['Carts', 'Carts.ProductsCart', 'Carts.ProductsCart.Product'])->get()[0];
-        foreach ($carts->carts as $cart) {
-            foreach ($cart->ProductsCart as $pc) {
-                $info['total_collected'] += $pc->quantity_sent * $pc->Product->price;
-            }
-            $countCarts ++;
-            if ($cart->state !== 0) {
-                $info['state'] = "En reparto";
-                $info['completed_carts'] ++;
-                if ($info['completed_carts'] === $this->Carts()->count()) {
-                    $info['state'] = "Completado";
-                }
-            }
-        }
-        return $info;*/
-        if ($this->Carts()->count() === 2) {
-            //dd($this->Carts);
-        }
         $info = [];
         $info['state'] = "En depósito";
         $info['total_carts'] = $this->Carts()->count();
@@ -65,7 +40,6 @@ class Route extends Model
             if ($cart->state !== 0) {
                 $info['state'] = "En reparto";
                 $info['completed_carts']++;
-                #dd($info['total_carts']);
                 if ($info['completed_carts'] === $info['total_carts']) {
                     $info['state'] = "Completado";
                 }
